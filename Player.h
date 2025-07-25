8a09b96297d5bc0786208fed7d99826482c72ec9
RyuJyashinn <k024g1044@g.neec.ac.jp>
2025-07-25 14:48:04 +0800

    添加项目文件。

---
+++ Player.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include"MyMath.h"
+
+class Player {
+private:
+
+	// 位置
+	Vector2 position_;
+
+	// 速さ
+	float speed_ = 2.0f;
+
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+	/// 右移動
+	void MoveRight();
+
+	/// 左移動
+	void MoveLeft();
+};
